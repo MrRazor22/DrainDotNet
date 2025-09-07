a898f13dbaaee03cfe518e783fb0f4bdf9a55da2
MrRazor22 <senthilvenkat22@gmail.com>
2025-09-08 02:44:30 +0530

    it fuking works!

--- DrainDotNet/Program.cs
+++ DrainDotNet/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string baseDir = AppContext.BaseDirectory;
+            string inputDir = Path.GetFullPath(Path.Combine(baseDir, @"..\..\..\data\loghub_2k\HDFS"));
+            string outputDir = Path.GetFullPath(Path.Combine(baseDir, @"..\..\..\result"));
+
+            string logFile = "HDFS_2k.log"; // The input log file name
+            string logFormat = "<Date> <Time> <Pid> <Level> <Component>: <Content>"; // HDFS log format
+
+
+            // Regular expression list for optional preprocessing (default: empty)
+            var regex = new List<string>
+{
+@"blk_(|-)[0-9]+", // block id
+@"(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)", // IP
+@"(?<=[^A-Za-z0-9])(\-?\+?\d+)(?=[^A-Za-z0-9])|[0-9]+$" // Numbers
+};
+
+
+            double st = 0.5; // Similarity threshold
+            int depth = 4; // Depth of all leaf nodes
+
+
+            var parser = new LogParser(logFormat, indir: inputDir, outdir: outputDir, depth: depth, st: st, rex: regex);
+            parser.Parse(logFile);
         }
     }
 }
